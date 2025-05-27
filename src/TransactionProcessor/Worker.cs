59589fffdf607238d4ea7830c2989735cacfaddf
jonathan <joni7840850@gmail.com>
2025-05-27 10:58:08 -0600

    Delete config dependecy

--- src/TransactionProcessor/Worker.cs
+++ src/TransactionProcessor/Worker.cs
@@ -7,12 +7,10 @@ namespace TransactionProcessor
     public class Worker : BackgroundService
     {
         private readonly ILogger<Worker> _logger;
-        private readonly IConfiguration _configuration;
 
-        public Worker(ILogger<Worker> logger, IConfiguration configuration)
+        public Worker(ILogger<Worker> logger)
         {
             _logger = logger;
-            _configuration = configuration;
         }
 
         protected override async Task ExecuteAsync(CancellationToken stoppingToken)
