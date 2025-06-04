3895a723a262faaa2769d3106212c251f9d1abad
jonathan <joni7840850@gmail.com>
2025-06-04 15:44:00 -0600

    Get key vault and secrets for config

--- src/FinancialMonitoring.Api/Program.cs
+++ src/FinancialMonitoring.Api/Program.cs
@@ -1,12 +1,32 @@
+using Azure.Identity;
 using FinancialMonitoring.Abstractions.Persistence;
 using FinancialMonitoring.Api.Services;
+using FinancialMonitoring.Models;
 
 var builder = WebApplication.CreateBuilder(args);
+
+builder.Services.Configure<CosmosDbSettings>(builder.Configuration.GetSection(AppConstants.CosmosDbConfigPrefix));
+
+var keyVaultUri = builder.Configuration["KEY_VAULT_URI"];
+if (!string.IsNullOrEmpty(keyVaultUri) && Uri.TryCreate(keyVaultUri, UriKind.Absolute, out var vaultUri))
+{
+    Console.WriteLine($"Attempting to load configuration from Azure Key Vault: {vaultUri}");
+    try
+    {
+        builder.Configuration.AddAzureKeyVault(vaultUri, new DefaultAzureCredential());
+        Console.WriteLine("Successfully configured to load secrets from Azure Key Vault.");
+    }
+    catch (Exception ex)
+    {
+        Console.WriteLine($"Error connecting to Azure Key Vault: {ex.Message}");
+    }
+}
+else
+{
+    Console.WriteLine("KEY_VAULT_URI not configured. Key Vault secrets will not be loaded.");
+}
 builder.Services.AddApplicationInsightsTelemetry();
 
-builder.Services.Configure<CosmosDbSettings>(
-    builder.Configuration.GetSection("CosmosDb")
-);
 builder.Services.AddSingleton<ITransactionQueryService, CosmosDbTransactionQueryService>();
 
 builder.Services.AddControllers();
