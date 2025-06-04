2b4717d479a8cd0d55f3f9407b5b84a7895a954f
jonathan <joni7840850@gmail.com>
2025-06-04 15:45:00 -0600

    Get key vault and secrets for config

--- src/TransactionSimulator/Program.cs
+++ src/TransactionSimulator/Program.cs
@@ -1,12 +1,37 @@
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using TransactionSimulator;
+using Azure.Identity;
+using Microsoft.Extensions.Configuration;
+using FinancialMonitoring.Models;
 
 public class Program
 {
     public static async Task Main(string[] args)
     {
         var builder = Host.CreateApplicationBuilder(args);
+        builder.Services.Configure<KafkaSettings>(builder.Configuration.GetSection(AppConstants.KafkaConfigPrefix));
+        builder.Services.Configure<ApplicationInsightsSettings>(builder.Configuration.GetSection(AppConstants.ApplicationInsightsConfigPrefix));
+
+        var keyVaultUri = builder.Configuration["KEY_VAULT_URI"];
+
+        if (!string.IsNullOrEmpty(keyVaultUri) && Uri.TryCreate(keyVaultUri, UriKind.Absolute, out var vaultUri))
+        {
+            Console.WriteLine($"Attempting to load configuration from Azure Key Vault: {vaultUri}");
+            try
+            {
+                builder.Configuration.AddAzureKeyVault(vaultUri, new DefaultAzureCredential());
+                Console.WriteLine("Successfully configured to load secrets from Azure Key Vault.");
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine($"Error connecting to Azure Key Vault: {ex.Message}");
+            }
+        }
+        else
+        {
+            Console.WriteLine("KEY_VAULT_URI not configured. Key Vault secrets will not be loaded.");
+        }
 
         builder.Services.AddApplicationInsightsTelemetryWorkerService();
 
