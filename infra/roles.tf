fa764bdffabf3da695bf9ec65bcac8d72bc0757d
jonathan <joni7840850@gmail.com>
2025-06-06 08:08:39 -0600

    Add roles file

---
+++ infra/roles.tf
@@ -0,0 +1,11 @@
+resource "azurerm_role_assignment" "app_sp_kv_secrets_user" {
+  scope                = azurerm_key_vault.kv.id
+  role_definition_name = "Key Vault Secrets User"
+  principal_id         = var.app_service_principal_object_id
+}
+
+resource "azurerm_role_assignment" "admin_user_kv_admin" {
+  scope                = azurerm_key_vault.kv.id
+  role_definition_name = "Key Vault Administrator"
+  principal_id         = data.azurerm_client_config.current.object_id
+}
