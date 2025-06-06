b9e58652dfe3be706052567464bf6b16a6b243ab
jonathan <joni7840850@gmail.com>
2025-06-06 08:08:17 -0600

    Add monitoring file

---
+++ infra/monitoring.tf
@@ -0,0 +1,19 @@
+resource "azurerm_log_analytics_workspace" "law" {
+  name                = "${var.resource_prefix}-law"
+  location            = azurerm_resource_group.rg.location
+  resource_group_name = azurerm_resource_group.rg.name
+  sku                 = "PerGB2018"
+  retention_in_days   = 30
+}
+
+resource "azurerm_application_insights" "appi" {
+  name                = "${var.resource_prefix}-appi"
+  location            = azurerm_resource_group.rg.location
+  resource_group_name = azurerm_resource_group.rg.name
+  workspace_id        = azurerm_log_analytics_workspace.law.id
+  application_type    = "web"
+  tags = {
+    environment = "development"
+    project     = "RealTimeFinancialMonitoring"
+  }
+}
