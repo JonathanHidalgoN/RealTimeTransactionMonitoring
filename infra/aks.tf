9343c8e40229eece2f1841272ecb466a20291f75
jonathan <joni7840850@gmail.com>
2025-06-10 16:15:49 -0600

    Add azure kubernetes service to infra

---
+++ infra/aks.tf
@@ -0,0 +1,23 @@
+resource "azurerm_kubernetes_cluster" "aks" {
+  name                = "${var.resource_prefix}-aks"
+  location            = azurerm_resource_group.rg.location
+  resource_group_name = azurerm_resource_group.rg.name
+  dns_prefix          = "${var.resource_prefix}-aks"
+  sku_tier            = "Free"
+
+  default_node_pool {
+    name       = "default"
+    node_count = 1
+    vm_size    = "Standard_B2s"
+  }
+
+  identity {
+    type = "SystemAssigned"
+  }
+
+  tags = {
+    environment = "development"
+    project     = "RealTimeFinancialMonitoring"
+  }
+}
+
