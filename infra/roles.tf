dc5ac904a1f572731565d415fd3ab5708664b7d1
jonathan <joni7840850@gmail.com>
2025-06-10 16:16:16 -0600

    add role for aks

--- infra/roles.tf
+++ infra/roles.tf
@@ -9,3 +9,11 @@ resource "azurerm_role_assignment" "admin_user_kv_admin" {
   role_definition_name = "Key Vault Administrator"
   principal_id         = var.admin_user_object_id
 }
+
+resource "azurerm_role_assignment" "aks_acr_pull" {
+  scope                = azurerm_container_registry.acr.id
+  role_definition_name = "AcrPull"
+  principal_id         = azurerm_kubernetes_cluster.aks.kubelet_identity[0].object_id
+  # azurerm_kubernetes_cluster.aks.identity[0].principal_id instead.
+}
+
