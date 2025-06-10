ad61f3b2585abf3a4093c29c8e61f6cdc312dde8
jonathan <joni7840850@gmail.com>
2025-06-10 16:16:06 -0600

    Add azure cosmos database to infra

---
+++ infra/cosmosdb.tf
@@ -0,0 +1,38 @@
+
+resource "azurerm_cosmosdb_account" "db" {
+  name                = "${var.resource_prefix}-cosmos"
+  location            = azurerm_resource_group.rg.location
+  resource_group_name = azurerm_resource_group.rg.name
+  offer_type          = "Standard"
+  kind                = "GlobalDocumentDB"
+
+  enable_free_tier    = true
+
+  consistency_policy {
+    consistency_level = "Session"
+  }
+
+  geo_location {
+    location          = azurerm_resource_group.rg.location
+    failover_priority = 0
+  }
+
+  tags = {
+    environment = "development"
+    project     = "RealTimeFinancialMonitoring"
+  }
+}
+
+resource "azurerm_cosmosdb_sql_database" "db" {
+  name                = "FinancialTransactionsDb"
+  resource_group_name = azurerm_resource_group.rg.name
+  account_name        = azurerm_cosmosdb_account.db.name
+}
+
+resource "azurerm_cosmosdb_sql_container" "transactions" {
+  name                  = "Transactions"
+  resource_group_name   = azurerm_resource_group.rg.name
+  account_name          = azurerm_cosmosdb_account.db.name
+  database_name         = azurerm_cosmosdb_sql_database.db.name
+  partition_key_path    = "/id"
+}
