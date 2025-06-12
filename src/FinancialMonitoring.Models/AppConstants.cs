646603a3396cf1fdf8f16719e59d6a7e7133e1a6
jonathan <joni7840850@gmail.com>
2025-06-12 08:28:22 -0600

    Add prefix for configs

--- src/FinancialMonitoring.Models/AppConstants.cs
+++ src/FinancialMonitoring.Models/AppConstants.cs
@@ -3,8 +3,10 @@ namespace FinancialMonitoring.Models;
 public static class AppConstants
 {
     public const string TransactionsTopicName = "transactions";
-
     public const string KafkaConfigPrefix = "Kafka";
     public const string ApplicationInsightsConfigPrefix = "ApplicationInsights";
     public const string CosmosDbConfigPrefix = "CosmosDb";
+    public const string MessagingConfigPrefix = "Messaging";
+    public const string EventHubsConfigPrefix = "EventHubs";
+    public const string KafkaDefaultName = "Kafka";
 }
