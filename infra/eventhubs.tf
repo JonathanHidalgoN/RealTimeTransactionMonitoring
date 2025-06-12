875392b9e8f9ac84441ecd7427409ed7354474da
jonathan <joni7840850@gmail.com>
2025-06-12 08:26:54 -0600

    Add evenhubs service

---
+++ infra/eventhubs.tf
@@ -0,0 +1,14 @@
+resource "azurerm_eventhub_namespace" "eh_namespace" {
+  name                = "${var.resource_prefix}-eh-ns"
+  location            = azurerm_resource_group.rg.location
+  resource_group_name = azurerm_resource_group.rg.name
+  sku                 = "Basic"
+}
+
+resource "azurerm_eventhub" "transactions" {
+  name                = "transactions"
+  namespace_name      = azurerm_eventhub_namespace.eh_namespace.name
+  resource_group_name = azurerm_resource_group.rg.name
+  partition_count     = 2
+  message_retention   = 1
+}
