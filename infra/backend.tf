5853f2f51b9c5837a3ce13946df02edf6ab29c58
jonathan <joni7840850@gmail.com>
2025-06-14 17:56:38 -0600

    Change azure account

--- infra/backend.tf
+++ infra/backend.tf
@@ -1,7 +1,7 @@
 terraform {
   backend "azurerm" {
     resource_group_name  = "finmon-rg"
-    storage_account_name = "finmontfstate82fsi5"
+    storage_account_name = "finmontfstate4nssob"
     container_name       = "tfstate"
     key                  = "realtimefinancialmonitoring.terraform.tfstate"
   }
