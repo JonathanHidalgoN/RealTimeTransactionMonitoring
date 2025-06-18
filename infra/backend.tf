fba4f6b00060b4959a289b1357f5382aeb4c8113
jonathan <joni7840850@gmail.com>
2025-06-18 14:45:52 -0600

    Add resources and limits to pods

--- infra/backend.tf
+++ infra/backend.tf
@@ -1,7 +1,7 @@
 terraform {
   backend "azurerm" {
     resource_group_name  = "finmon-rg"
-    storage_account_name = "finmontfstate4nssob"
+    storage_account_name = "finmontfstate5a9syt"
     container_name       = "tfstate"
     key                  = "realtimefinancialmonitoring.terraform.tfstate"
   }
