987d0b63972e864817b1847405a00af26d1b54f9
jonathan <joni7840850@gmail.com>
2025-06-19 11:07:00 -0600

    Add constant

--- src/FinancialMonitoring.Models/AppConstants.cs
+++ src/FinancialMonitoring.Models/AppConstants.cs
@@ -3,6 +3,7 @@ namespace FinancialMonitoring.Models;
 public static class AppConstants
 {
     public const string TransactionsTopicName = "transactions";
+    public const string AnomaliesTopicName = "anomalies";
     public const string KafkaConfigPrefix = "Kafka";
     public const string ApplicationInsightsConfigPrefix = "ApplicationInsights";
     public const string CosmosDbConfigPrefix = "CosmosDb";
