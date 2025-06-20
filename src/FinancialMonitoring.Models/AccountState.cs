e678995bfa999d6ba22d067fb196fed9bf9a6fb5
jonathan <joni7840850@gmail.com>
2025-06-20 14:39:25 -0600

    Add model to represent account state

---
+++ src/FinancialMonitoring.Models/AccountState.cs
@@ -0,0 +1,7 @@
+namespace FinancialMonitoring.Models;
+
+public class AccountStats
+{
+    public int TransactionCount { get; set; } = 0;
+    public double AverageTransactionAmount { get; set; } = 0.0;
+}
