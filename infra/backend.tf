f2637adfb55d19ff30da563a66053f3d7a513028
jonathan <joni7840850@gmail.com>
2025-06-22 17:22:52 -0600

    New account name

--- infra/backend.tf
+++ infra/backend.tf
@@ -1,7 +1,7 @@
 terraform {
   backend "azurerm" {
     resource_group_name  = "finmon-rg"
-    storage_account_name = "finmontfstate5a9syt"
+    storage_account_name = "finmontfstate55mbn8"
     container_name       = "tfstate"
     key                  = "realtimefinancialmonitoring.terraform.tfstate"
   }
