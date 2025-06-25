554c0763588c8da403fadda5a85a7311da4fc171
jonathan <joni7840850@gmail.com>
2025-06-25 15:57:43 -0600

    Change to display in mx

--- src/FinancialMonitoring.WebApp/Program.cs
+++ src/FinancialMonitoring.WebApp/Program.cs
@@ -4,30 +4,28 @@ using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
 using MudBlazor.Services;
 using System.Net.Http;
 using FinancialMonitoring.WebApp.Services;
+using System.Globalization;
 
 public class Program
 {
     public static async Task Main(string[] args)
     {
+        var culture = new CultureInfo("es-MX");
+        CultureInfo.DefaultThreadCurrentCulture = culture;
+        CultureInfo.DefaultThreadCurrentUICulture = culture;
+
         var builder = WebAssemblyHostBuilder.CreateDefault(args);
         builder.Configuration.AddJsonFile("appsettings.json");
 
         builder.RootComponents.Add<App>("#app");
         builder.RootComponents.Add<HeadOutlet>("head::after");
 
-        // --- REPLACE the old AddScoped line WITH THIS BLOCK ---
-
-        // Register ApiClientService and configure a typed HttpClient for it.
-        // This is the standard "HttpClientFactory" pattern.
         builder.Services.AddHttpClient<ApiClientService>(client =>
         {
-            // Set the base address for all requests made by this client
             client.BaseAddress = new Uri(builder.Configuration["ApiBaseUrl"]!);
 
-            // Add the API Key header to every request made by this client
             client.DefaultRequestHeaders.Add("X-Api-Key", builder.Configuration["ApiKey"]);
         });
-        // ----------------------------------------------------
 
         builder.Services.AddMudServices();
 
