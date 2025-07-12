f36195f82776053244c8c1a43b49bc1b4c03a3e3
jonathan <joni7840850@gmail.com>
2025-07-12 16:19:29 -0600

    Reduce tier of machines

--- infra/aks.tf
+++ infra/aks.tf
@@ -17,7 +17,7 @@ resource "azurerm_kubernetes_cluster" "aks" {
   default_node_pool {
     name                = "default"
     node_count          = 1
-    vm_size             = "Standard_B2s"
+    vm_size             = "Standard_B1s"
     enable_auto_scaling = true
     min_count           = 1
     max_count           = 2
