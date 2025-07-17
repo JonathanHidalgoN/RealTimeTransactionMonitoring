e00a93883354c8413863b05dbeb1d4047a197cbc
jonathan <joni7840850@gmail.com>
2025-07-17 17:55:55 -0600

    Temp changes

--- infra/backend.tf
+++ infra/backend.tf
@@ -1,7 +1,7 @@
 terraform {
   backend "azurerm" {
-    resource_group_name  = "finmon"
-    storage_account_name = "finmontfstate45qna2"
+    resource_group_name  = "finmon-rg"
+    storage_account_name = "finmontfstate2f0vu6"
     container_name       = "tfstate"
     key                  = "realtimefinancialmonitoring.terraform.tfstate"
   }
