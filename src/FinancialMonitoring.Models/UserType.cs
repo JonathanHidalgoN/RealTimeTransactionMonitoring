a34b2445531f17830ba288ce4fe7295fb19c1339
jonathan <joni7840850@gmail.com>
2025-07-23 16:19:00 -0600

    Add enum for user type

---
+++ src/FinancialMonitoring.Models/UserType.cs
@@ -0,0 +1,16 @@
+namespace FinancialMonitoring.Models;
+
+/// <summary>
+/// Represents different user profile types with distinct spending patterns
+/// </summary>
+public enum UserType
+{
+    Student,
+    YoungProfessional,
+    FamilyPerson,
+    MiddleAged,
+    Retiree,
+    HighNetWorth,
+    SmallBusiness,
+    Freelancer
+}
