72ba7cf5086392f5988616fcd7954d1eb91b4e06
jonathan <joni7840850@gmail.com>
2025-09-01 13:50:49 -0600

    Add using statement

--- src/FinancialMonitoring.Abstractions/IJwtTokenService.cs
+++ src/FinancialMonitoring.Abstractions/IJwtTokenService.cs
@@ -1,3 +1,4 @@
+using FinancialMonitoring.Models;
 namespace FinancialMonitoring.Abstractions;
 
 /// <summary>
@@ -9,22 +10,22 @@ public interface IJwtTokenService
     /// Generates a new access token for the user
     /// </summary>
     string GenerateAccessToken(AuthUser user);
-    
+
     /// <summary>
     /// Generates a new refresh token
     /// </summary>
     string GenerateRefreshToken();
-    
+
     /// <summary>
     /// Validates a refresh token and returns the associated user ID
     /// </summary>
     int? ValidateRefreshToken(string refreshToken);
-    
+
     /// <summary>
     /// Invalidates a refresh token (for logout)
     /// </summary>
     void InvalidateRefreshToken(string refreshToken);
-    
+
     /// <summary>
     /// Gets the expiration time for access tokens
     /// </summary>
