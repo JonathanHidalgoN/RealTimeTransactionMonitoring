e8c397bef71a7fa3c7240f24aaefac5c7aafdbbf
jonathan <joni7840850@gmail.com>
2025-09-29 22:42:45 -0600

    Refactor: Move monitoring infra resouces to monitoring terraform module

---
+++ platform/terraform/modules/monitoring/outputs.tf
@@ -0,0 +1,31 @@
+output "log_analytics_workspace_id" {
+  description = "The ID of the Log Analytics workspace"
+  value       = azurerm_log_analytics_workspace.main.id
+}
+
+output "log_analytics_workspace_name" {
+  description = "The name of the Log Analytics workspace"
+  value       = azurerm_log_analytics_workspace.main.name
+}
+
+output "application_insights_id" {
+  description = "The ID of the Application Insights instance"
+  value       = azurerm_application_insights.main.id
+}
+
+output "application_insights_instrumentation_key" {
+  description = "The instrumentation key of Application Insights"
+  value       = azurerm_application_insights.main.instrumentation_key
+  sensitive   = true
+}
+
+output "application_insights_connection_string" {
+  description = "The connection string of Application Insights"
+  value       = azurerm_application_insights.main.connection_string
+  sensitive   = true
+}
+
+output "resource_group_name" {
+  description = "The name of the monitoring resource group"
+  value       = azurerm_resource_group.monitoring.name
+}
