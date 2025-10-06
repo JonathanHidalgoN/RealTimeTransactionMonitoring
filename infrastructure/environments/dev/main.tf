08bb92a27f12d50fa0a3f884a167593d4703c5b8
jonathan <joni7840850@gmail.com>
2025-10-06 15:37:26 -0600

    Fix: Add more env variables needed at runtime

--- infrastructure/environments/dev/main.tf
+++ infrastructure/environments/dev/main.tf
@@ -60,19 +60,27 @@ module "data" {
 module "container_apps" {
   source = "../../modules/container-apps"
 
-  environment                  = local.environment
-  resource_prefix              = var.resource_prefix
-  location                     = var.location
-  resource_group_name          = azurerm_resource_group.main.name
-  log_analytics_workspace_id   = module.monitoring.log_analytics_workspace_id
-  acr_login_server             = data.terraform_remote_state.shared.outputs.acr_login_server
-  acr_admin_username           = data.terraform_remote_state.shared.outputs.acr_admin_username
-  acr_admin_password           = data.terraform_remote_state.shared.outputs.acr_admin_password
-  cosmos_connection_string     = module.data.cosmos_connection_string
-  eventhub_connection_string   = module.data.eventhub_connection_string
-  storage_connection_string    = module.data.storage_connection_string
+  environment                    = local.environment
+  resource_prefix                = var.resource_prefix
+  location                       = var.location
+  resource_group_name            = azurerm_resource_group.main.name
+  log_analytics_workspace_id     = module.monitoring.log_analytics_workspace_id
+  acr_login_server               = data.terraform_remote_state.shared.outputs.acr_login_server
+  acr_admin_username             = data.terraform_remote_state.shared.outputs.acr_admin_username
+  acr_admin_password             = data.terraform_remote_state.shared.outputs.acr_admin_password
+  cosmos_connection_string       = module.data.cosmos_connection_string
+  cosmos_endpoint                = module.data.cosmos_endpoint
+  cosmos_primary_key             = module.data.cosmos_primary_key
+  cosmos_database_name           = module.data.cosmos_database_name
+  cosmos_container_name          = module.data.cosmos_container_name
+  cosmos_partition_key_path      = module.data.cosmos_partition_key_path
+  eventhub_connection_string     = module.data.eventhub_connection_string
+  storage_connection_string      = module.data.storage_connection_string
   app_insights_connection_string = module.monitoring.app_insights_connection_string
-  tags                         = local.common_tags
+  key_vault_uri                  = module.security.key_vault_uri
+  managed_identity_id            = module.security.managed_identity_id
+  managed_identity_client_id     = module.security.managed_identity_client_id
+  tags                           = local.common_tags
 }
 
 module "frontend" {
